d336708a5ad4afa8e1891093fbdf54e4dbb20e58
Piash-Akbar <sknafisakbar@gmail.com>
2024-10-28 20:42:38 +0600

    123

--- Bookify/src/pages/ViewOrderDetails.jsx
+++ Bookify/src/pages/ViewOrderDetails.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useFirebase } from '../context/Firebase'
 
 const ViewOrderDetails = () => {
+    const params = useParams()
+    console.log(params)
+
+    const firebase = useFirebase()
+    const [orders,setOrders] = useState([])
+
+    useEffect(()=>{
+        firebase.getOrders(params.bookId).then((order)=> setOrders(order.docs))
+    },[firebase])
+
   return (
-    <div>ViewOrderDetails</div>
+    <div>
+        <h1>Orders</h1>
+        {
+            orders.map((order)=>{
+                const data =order.data()
+                return(
+                    <div style={{border: "1px solid black", margin: "5px", padding:"1rem"}}>
+                        <p>Ordered By: {data.displayName}</p>
+                        <p>Qty: {data.qty}</p>
+                        <p>Email: {data.userEmail}</p>
+                    </div>
+                )
+            })
+        }
+    </div>
   )
 }
 
